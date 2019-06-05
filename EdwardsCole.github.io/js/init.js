7bedb9ee9cc4769a1228a7588c95be58036ed014
PembertonStacey <43327999+PembertonStacey@users.noreply.github.com>
2019-06-05 06:53:29 -0700

    Add files via upload

---
+++ EdwardsCole.github.io/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $(".dropdown-trigger").dropdown();
+    $('.carousel').carousel({
+      indicators: true
+    });
+  });
+
+})(jQuery); // end of jQuery name space
